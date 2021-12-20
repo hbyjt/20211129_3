ea028198926645d24d3531b102c3acb3cb7b6ce7
hbyjt <95209304+hbyjt@users.noreply.github.com>
2021-12-20 15:28:04 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/arwt_text.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/123.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
